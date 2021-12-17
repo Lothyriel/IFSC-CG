3e8d911bfe7827927dd594a08fa6fc3246fb7649
Lothyriel <fastjonh@gmail.com>
2021-12-17 14:00:29 -0300

    att
    Adicionado:
    
    --responsividade ao alterar o redimensionar a tela
    
    --opcao click esquerdo para adicionar os pontos da curva (ainda esta meio bugado)
    
    --opcao enter para iniciar a renderizacao da curva
    
    --opcao esc para resetar os pontos da curva

--- src/main.js
+++ src/main.js
@@ -1,28 +1,34 @@
 "use strict";
 class Canvas {
-    constructor(scene, camera, render) {
+    constructor(scene, camera, render, element) {
         this.scene = scene;
         this.camera = camera;
         this.render = render;
         this.entities = {};
         this.keys = {};
         this.up = new THREE.Vector3(1, 0, 0);
+        this.started = false;
+        this.element = element;
+        this.curveMainPoints = [];
     }
     draw() {
         this.render.render(this.scene, this.camera);
 
-        treatInputs();
-        this.entities.car.draw();
+        treatKeyInputs();
+
+        Object.values(this.entities).forEach(element => {
+            if (typeof element.draw === 'function')
+                element.draw();
+        });
 
         requestAnimationFrame(this.draw.bind(this));
     }
 }
 
 class Car {
-    constructor(maxSpeed, curve, step) {
-        this.maxSpeed = maxSpeed;
+    constructor(curve, step) {
         this.curve = curve;
-        this.counter = 0
+        this.counter = 0;
         this.cube = this.createCube();
         this.step = step;
     }
@@ -35,24 +41,22 @@ class Car {
         return cube;
     }
     draw() {
-        console.log(this.counter +" "+  this.cube.position.x + " " + this.cube.position.y);
+        console.log(`t: ${this.counter} | x: ${this.cube.position.x} y: ${this.cube.position.y}`);
+
         let point = this.curve.getPointAt(this.counter);
         this.cube.position.set(point.x, point.y, 0);
-        this.adjustAngle()
+        this.adjustAngle();
     }
     adjustAngle() {
         let tangent = this.curve.getTangentAt(this.counter).normalize();
         tangent = new THREE.Vector3(tangent.x, tangent.y, 0);
 
-        let axis = new THREE.Vector3(); 
+        let axis = new THREE.Vector3();
         axis.crossVectors(canvas.up, tangent).normalize();
 
         let radians = Math.acos(canvas.up.dot(tangent));
 
         this.cube.quaternion.setFromAxisAngle(axis, radians);
-    }
-    move(step) {
-        this.counter += step;
     }
     moveLeft() {
         if (this.counter - this.step < 0)
@@ -65,20 +69,14 @@ class Car {
             return;
         this.move(this.step);
     }
+    move(step) {
+        this.counter += step;
+    }
 }
 
-function createCurve(precision) {
+function createCurve(precision, mainPoints) {
     let lineMaterial = new THREE.LineBasicMaterial({ color: 0xFFFFFF });
-    let originalPoints = [
-        new THREE.Vector3(-1, 0, 0),
-        new THREE.Vector3(-0.5, 0.5, 0),
-        new THREE.Vector3(0, 0, 0),
-        new THREE.Vector3(0.5, -0.5, 0),
-        new THREE.Vector3(1, 0, 0),
-        new THREE.Vector3(1.5, 0.5, 0),
-        new THREE.Vector3(2, 0, 0)
-    ];
-    let curve = new THREE.SplineCurve(originalPoints);
+    let curve = new THREE.SplineCurve(mainPoints);
     let points = curve.getPoints(precision);
 
     let lineGeometry = new THREE.BufferGeometry().setFromPoints(points);
@@ -89,15 +87,14 @@ function createCurve(precision) {
 }
 
 function drawInitialPoints(points) {
-    let pointMaterial = new THREE.PointsMaterial({ size: 10, sizeAttenuation: false });
+    for (let p of points)
+        canvas.scene.add(renderPoint(new THREE.Vector3(p.x, p.y, p.z)));
+}
 
-    for (let p of points) {
-        let coord = [];
-        coord.push(new THREE.Vector3(p.x, p.y, p.z));
-        let pointGeometry = new THREE.BufferGeometry().setFromPoints(coord);
-        let renderedPoint = new THREE.Points(pointGeometry, pointMaterial);
-        canvas.scene.add(renderedPoint);
-    }
+function renderPoint(vector3) {
+    let pointMaterial = new THREE.PointsMaterial({ size: 10, sizeAttenuation: false });
+    let pointGeometry = new THREE.BufferGeometry().setFromPoints([vector3]);
+    return new THREE.Points(pointGeometry, pointMaterial);
 }
 
 function configCanvas() {
@@ -112,46 +109,101 @@ function configCanvas() {
     let canvasElement = render.domElement;
     document.body.appendChild(canvasElement);
 
-    let ambientLight = new THREE.AmbientLight(0x333333);
-    scene.add(ambientLight);
-
-    let lightPoint = new THREE.PointLight(0x888888);
-    lightPoint.position.set(2, 2, 4);
-    scene.add(lightPoint);
-
     let _ = new THREE.OrbitControls(camera, render.domElement);
 
-    return new Canvas(scene, camera, render)
+    return new Canvas(scene, camera, render, canvasElement);
 }
 
 function configInputs() {
     document.onkeydown = function (evt) {
         canvas.keys[evt.key] = true;
+        console.log(evt.key);
     }
     document.onkeyup = function (evt) {
         canvas.keys[evt.key] = false;
     }
+    document.onclick = function (evt) {
+        treatMouseClick(evt);
+    }
+    window.addEventListener('resize', onWindowResize, false);
 }
 
-function treatInputs() {
-    let car = canvas.entities.car;
+function treatKeyInputs() {
+    let car = canvas.entities["car"];
     if (canvas.keys["ArrowLeft"])
         car.moveLeft();
     if (canvas.keys["ArrowRight"])
         car.moveRight();
+    if (canvas.keys["Enter"])
+        configEntities();
+    if (canvas.keys["Escape"])
+        reset();
 }
 
-function main() {    
-    configInputs();
+function treatMouseClick(evt) {
+    if (canvas.started)
+        return;
 
-    let curve = createCurve(100);
-    canvas.entities.curve = curve;
+    let x = (evt.clientX / window.innerWidth) * 2 - 1;
+    let y = -(evt.clientY / window.innerHeight) * 2 + 1;
 
-    canvas.entities.car = new Car(10, canvas.entities.curve, 0.01);
+    let point = new THREE.Vector3(x, y, 0);
+
+    canvas.curveMainPoints.push(point);
+    canvas.scene.add(renderPoint(point));
+}
+
+function onWindowResize() {
+    canvas.camera.aspect = window.innerWidth / window.innerHeight;
+    canvas.camera.updateProjectionMatrix();
+    canvas.render.setSize(window.innerWidth, window.innerHeight);
+}
+
+function configEntities() {
+    if (canvas.curveMainPoints.length < minCurvePoints) {
+        console.log(`Menos de ${minCurvePoints} pontos iniciais`);
+        return;
+    }
+    if (canvas.started)
+        return;
+    let curve = createCurve(curvePrecision, canvas.curveMainPoints);
+    canvas.entities["curve"] = curve;
+
+    canvas.entities["car"] = new Car(curve, carStep);
 
     drawInitialPoints(curve.points);
+
+    drawLights();
+
+    canvas.started = true;
+}
+
+function drawLights() {
+    let ambientLight = new THREE.AmbientLight(0x333333);
+    canvas.scene.add(ambientLight);
+
+    let lightPoint = new THREE.PointLight(0x888888);
+    lightPoint.position.set(2, 2, 4);
+    canvas.scene.add(lightPoint);
+}
+
+function reset() {
+    let scene = canvas.scene;
+    scene.remove.apply(scene, scene.children);
+
+    configEntities();
+
+    canvas.curveMainPoints = [];
+    canvas.started = false;
+}
+
+function main() {
+    configInputs();
 
     canvas.draw();
 }
 
+const minCurvePoints = 3;
+const curvePrecision = 200;
+const carStep = 0.01;
 var canvas = configCanvas();
